982792b5d0f806ce1310544c16e83562574f1a28
Hyunwoo-Seo <shw1801@naver.com>
2025-05-20 15:25:55 +0900

    fix : package.json 에 script 추가

--- prisma/generated/edge.js
+++ prisma/generated/edge.js
@@ -306,7 +306,8 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null
+    "rootEnvPath": null,
+    "schemaEnvPath": "../../.env"
   },
   "relativePath": "..",
   "clientVersion": "6.8.2",
